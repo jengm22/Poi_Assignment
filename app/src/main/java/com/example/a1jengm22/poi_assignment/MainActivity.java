03e9511b207521fc762398a3ec4c920eb19805b0
Mahtarr Jeng <Mahtarr Jeng>
2017-05-08 15:10:00 +0100

    Initial commit    task 1 complete

---
+++ app/src/main/java/com/example/a1jengm22/poi_assignment/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.a1jengm22.poi_assignment;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    MapView mv;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+
+        // This line sets the user agent, a requirement to download OSM maps
+        org.osmdroid.config.Configuration.getInstance().load
+                (this, PreferenceManager.getDefaultSharedPreferences(this));
+        setContentView(R.layout.activity_main);
+
+        mv = (MapView)findViewById(R.id.map1);
+
+        mv.setBuiltInZoomControls(true);
+        mv.getController().setZoom(14);
+        mv.getController().setCenter(new GeoPoint(51.05,-0.72));
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.addNewPoi)
+        {
+            // react to the menu item being selected...
+            // System.exit(0); this code closes the app
+
+            Intent intent = new Intent(this, NewPoi.class);
+            startActivityForResult(intent, 0);
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+}
+
---
+++ app/src/main/java/com/example/a1jengm22/poi_assignment/NewPoi.java
@@ -0,0 +1,13 @@
+package com.example.a1jengm22.poi_assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewPoi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_poi);
+    }
+}
