070b761a3c66214757a8caf8a5f838cd72b34feb
1jengm22 <mahtarrjeng@gmail.com>
2017-05-11 14:18:23 +0100

    task 3

--- app/src/main/java/com/example/a1jengm22/poi_assignment/MainActivity.java
+++ app/src/main/java/com/example/a1jengm22/poi_assignment/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.a1jengm22.poi_assignment;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,14 +12,27 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
 
-public class MainActivity extends AppCompatActivity
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity
 {
 
     MapView mv;
+
+    ItemizedIconOverlay<OverlayItem> items;
+    ItemizedIconOverlay.OnItemGestureListener<OverlayItem> markerGestureListener;
 
     /** Called when the activity is first created. */
     @Override
@@ -49,10 +65,68 @@ public class MainActivity extends AppCompatActivity
 
             return true;
         }
-        else
+       return false;
+
+    }
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+
+        if(resultCode == RESULT_OK)
         {
-            return false;
+
+            if (requestCode == 0)
+            {
+                Bundle extras=intent.getExtras();
+                String name = extras.getString("com.example.a1jengm22.poiName");
+                String type = extras.getString("com.example.a1jengm22.poiType");
+                String desc = extras.getString("com.example.a1jengm22.poiDesc");
+
+                double latitude = mv.getMapCenter().getLatitude();
+                double longitude = mv.getMapCenter().getLongitude();
+
+                //to link listener to the overlay layer
+                items = new ItemizedIconOverlay<OverlayItem>(this, new ArrayList<OverlayItem>(), markerGestureListener);
+                OverlayItem banjul = new OverlayItem(name, type, desc, new GeoPoint(latitude, longitude));
+
+                items.addItem(banjul);
+                mv.getOverlays().add(items);
+
+                //saving the poi added
+
+                try {
+                    PrintWriter pw =
+                            new PrintWriter(new FileWriter(Environment.getExternalStorageDirectory().getAbsolutePath() + "/data.txt",true));
+
+
+                    for(int i=0;  i<items.size(); i++)
+                    {
+                        OverlayItem place = items.getItem(i);
+                        pw.println(place.getTitle()+","+ place.getSnippet()+ ","+ place.getPoint().getLatitude()+ ","+ place.getPoint().getLongitude());
+                    }
+
+
+
+                    pw.close(); // close the file to ensure data is flushed to file
+                } catch (IOException e) {
+                    new AlertDialog.Builder(this).setMessage("ERROR: " + e).
+                            setPositiveButton("OK", null).show();
+                }
+
+            }
+            else if (requestCode == 1)
+            {
+
+                Bundle extras=intent.getExtras();
+                double longitude  = extras.getDouble("com.example.longitude");
+                double latitude= extras.getDouble("com.example.latitude");
+                extras.getDouble("com.example.longitude");
+                mv.getController().setCenter(new GeoPoint(latitude,longitude));
+            }
+
+
         }
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu)
--- app/src/main/java/com/example/a1jengm22/poi_assignment/NewPoi.java
+++ app/src/main/java/com/example/a1jengm22/poi_assignment/NewPoi.java
@@ -1,13 +1,15 @@
 package com.example.a1jengm22.poi_assignment;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class NewPoi extends AppCompatActivity implements View.OnClickListener
+public class NewPoi extends Activity implements View.OnClickListener
 {
 
     @Override
@@ -22,6 +24,15 @@ public class NewPoi extends AppCompatActivity implements View.OnClickListener
 
     public void onClick(View v)
     {
+        EditText poiName = (EditText)findViewById(R.id.poiName);
+        EditText poiType = (EditText)findViewById(R.id.poiType);
+        EditText poiDesc = (EditText)findViewById(R.id.poiDesc);
+
+        String name = poiName.getText().toString();
+        String type = poiType.getText().toString();
+        String desc = poiDesc.getText().toString();
+
+
         Intent intent = new Intent();
         Bundle bundle=new Bundle();
         boolean addNewPoi=false;
@@ -29,7 +40,10 @@ public class NewPoi extends AppCompatActivity implements View.OnClickListener
         {
             addNewPoi=true;
         }
-        bundle.putBoolean("com.example.newPoi",addNewPoi);
+        bundle.putString("com.example.a1jengm22.poiName",name);
+        bundle.putString("com.example.a1jengm22.poiType",type);
+        bundle.putString("com.example.a1jengm22.poiDesc",desc);
+
         intent.putExtras(bundle);
         setResult(RESULT_OK,intent);
         finish();
